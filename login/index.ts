5fef799447ef68e5d8820cfe92003936973c9826
jordan <jordanelcracken@gmail.com>
2025-06-01 10:38:45 -0500

    Feat: update

--- login/index.ts
+++
@@ -1,8 +0,0 @@
-import { loginApp } from "./src/app";
-
-const PORT = process.env.PORT || 4001
-const app = loginApp
-
-app.listen(PORT, () => {
-  console.log(`App is runnin on http://localhost:${PORT}/`)
-})
